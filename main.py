57bf4a5f114c8c29035ee84af5ce74630420f8ee
jasoncot <jasoncot@gmail.com>
2025-01-06 20:35:30 -0800

    L11: completed functionality

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def count_characters(text):
     map = {}
     for char in text.lower():
+        if not char in letters_in_sequence():
+            continue
+
         if not char in map:
             map[char] = 0
         map[char] += 1
@@ -10,10 +13,46 @@ def count_characters(text):
 def break_into_words(text):
     return text.split()
 
-def main():
-    with open("./books/frankenstein.txt") as f:
+def read_file(filename):
+    file_contents = None
+
+    with open(filename) as f:
         file_contents = f.read()
-        print(len(break_into_words(file_contents)))
-        print(count_characters(file_contents))
+
+    return file_contents
+
+def letters_in_sequence():
+    return 'abcdefghijklmnopqrstuvwxyz'
+
+def letters_by_occurance(map):
+    list_of_pairs = []
+    for key in map:
+        list_of_pairs.append((map[key], key))
+    list_of_pairs.sort(key=lambda pair: pair[0], reverse=True)
+
+    sorted_keys = []
+    for key in list_of_pairs:
+        sorted_keys.append(key[1])
+
+    return sorted_keys
+
+def print_report(filename):
+    file_contents = read_file(filename)
+    if file_contents == None:
+        raise Exception(f"unable to open file {filename}")
+    
+    word_count = len(break_into_words(file_contents))
+    char_map = count_characters(file_contents)
+
+    print(f"--- begin report of {filename} ---")
+    print(f"{word_count} words found in the document")
+    print("")
+    for char in letters_by_occurance(char_map):
+        print(f"The '{char}' character was found {char_map[char]} times")
+    print("--- End report ---")
+    
+
+def main():
+    print_report("./books/frankenstein.txt")
 
 main()
